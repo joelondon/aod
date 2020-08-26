9051c8a064ae755bcea0e855ffb6225051075265
joseph.leach <joseph.leach@london.gov.uk>
2020-08-26 10:17:35 +0100

    Updates

---
+++ precache-manifest.6308ac7d6721a6752dbf64c7390d6d6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7620f82576bd6567ccccae0744f03c46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebdeb13877a73a20e869",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "85705fb5adf7d4876a2f",
+    "url": "/static/js/2.fc71a8d6.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/static/js/2.fc71a8d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebdeb13877a73a20e869",
+    "url": "/static/js/main.366490c3.chunk.js"
+  },
+  {
+    "revision": "f038c010de0c47369915",
+    "url": "/static/js/runtime-main.0b0a5f46.js"
+  }
+]);
